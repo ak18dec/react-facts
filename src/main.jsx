dd6c305819d44cc8d6197dfc8d1e0a50e5d29d36
Ankit <ankit.dayal1992@gmail.com>
2025-04-12 00:50:45 +0530

    Main and Navbar Components Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
+import App from './App'
+
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  temporaryMain()
+  <App />
 )
-
-
-function temporaryMain() {
-  return (
-    <main>
-      <img src="/src/assets/react.svg" alt="React Logo" />
-      <h1>Fun Facts about React!</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200k starts on Github</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
-  )
-}
